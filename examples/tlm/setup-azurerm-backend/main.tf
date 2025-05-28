9dcd4bf056e52271b651cc74b49bdf3e0b0a4d35
Kelsey Wong <kelsey.wong@cleanlab.ai>
2025-05-28 15:00:57 -0700

    setup-azurerm-backend works after importing some existing resources

--- examples/tlm/setup-azurerm-backend/main.tf
+++ examples/tlm/setup-azurerm-backend/main.tf
@@ -3,10 +3,12 @@ provider "azurerm" {
 }
 
 locals {
-    resource_group_name = "tlm-${var.environment}-rg"
+    environment = "staging"
+    resource_group_name = "tfstate-rg"
+    storage_account_name = "tlmtestinfratfstate"
 
     tags = {
-        environment = var.environment
+        environment = local.environment
         project = "tlm"
         terraform = "true"
     }
@@ -15,12 +17,12 @@ locals {
 module "bootstrap" {
     source               = "../../../tlm/bootstrap"
     resource_group_name  = local.resource_group_name
-    location             = var.location
-    environment          = var.environment
-    storage_account_name = var.storage_account_name
-    storage_account_id   = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${var.storage_account_name}"
-    container_name       = var.container_name
-    accessor_object_id   = var.accessor_object_id
+    location             = "eastus"
+    environment          = local.environment
+    storage_account_name = local.storage_account_name
+    storage_account_id   = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.Storage/storageAccounts/${local.storage_account_name}"
+    container_name       = "tfstate"
+    accessor_object_id   = "b0ae2faf-4ecc-44ad-bcd7-b9462845640e"
     tags                 = local.tags
 }
 
