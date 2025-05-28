98206644e348314a4b37a4c56bfa0482c03cb884
Kelsey Wong <kelsey.wong@cleanlab.ai>
2025-05-28 14:30:39 -0700

    clean up infra example

--- examples/tlm/infra/main.tf
+++ examples/tlm/infra/main.tf
@@ -3,33 +3,34 @@ provider "azurerm" {
 }
 
 locals {
-    environment = var.environment
-    entity = var.entity
-    location = var.location
-
-    tags = {
-        environment = local.environment
-        project = "tlm"
-        terraform = "true"
-    }
+    environment = "staging"
 }
 
 module "infra" {
     source = "../../../tlm/infra"
 
     environment = local.environment
-    entity = local.entity
-    location = local.location
+    entity = "your-company-name"
+    location = "eastus"
 
     create_openai_service = false
     openai_service_name = "tlm-openai"
     openai_service_resource_group_name = "tlm-staging-rg"
     
     create_imagepull_app_registration = false
-    existing_imagepull_app_registration = var.existing_imagepull_app_registration
+    existing_imagepull_app_registration = {
+        id = "/applications/c31c140c-1fbd-41c4-a088-428841cd560e"
+        client_id = "c2a065ca-b62a-4647-9622-40cf283de0d1"
+        tenant_id = "d3da5a95-4d1d-48aa-a0a6-87fe75942484"
+        password = ""
+    }
 
     default_completion_model = "azure/gpt-4o-mini"
     default_embedding_model = "azure/text-embedding-3-small"
 
-    tags = local.tags
+    tags = {
+        environment = local.environment
+        project = "tlm"
+        terraform = "true"
+    }
 }
