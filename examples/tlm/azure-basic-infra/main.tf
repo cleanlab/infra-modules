8dede9495896a05c223fc57b4f2c1a3342d7617f
Kelsey Wong <kelsey.wong@cleanlab.ai>
2025-05-30 16:44:06 -0700

    Final tweaks to readmes (#6)
    * update default tags
    
    * add changelog and development
    
    * update wording
    
    * create template for setting UAMI env vars, use git tag for infra module refs
    
    * remove uami script

--- examples/tlm/azure-basic-infra/main.tf
+++ examples/tlm/azure-basic-infra/main.tf
@@ -8,7 +8,7 @@ locals {
 }
 
 module "infra" {
-    source = "../../../tlm/infra"
+    source = "git::https://github.com/cleanlab/infra-modules.git//tlm/infra?ref=v1.0.0"
 
     environment = local.environment
     entity = "your-company-name"
---
+++ examples/tlm/setup-azurerm-backend/set_uami_environment.template.sh
@@ -0,0 +1,4 @@
+export ARM_USE_MSI=true
+export ARM_CLIENT_ID=<your-client-id>
+export ARM_TENANT_ID=<your-tenant-id>
+export ARM_SUBSCRIPTION_ID=<your-subscription-id> 
